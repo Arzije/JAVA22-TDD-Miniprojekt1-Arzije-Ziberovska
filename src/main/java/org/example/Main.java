da723cc687464ccacef68e7f62c6c4427bc34b21
Arzije Ziberovska <arzije.ziberovska@gmail.com>
2023-11-17 01:34:05 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+//        Triangle triangle = new Triangle(1,2,3);// vf går det inte att sätta 1,2,3??
+        Triangle triangle = new Triangle();
+        triangle.getUserInput();
+        triangle.getCurrent_type();
+    }
+}
---
+++ src/test/java/org/example/TriangleTest.java
@@ -0,0 +1,154 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleTest {
+
+    private String[] input;
+    private ByteArrayInputStream inputArray;
+    private Triangle triangle;
+
+
+    // testa fel datatyp
+    @Test
+    @DisplayName("Test for invalid datatype and length input, throw IllegalArgument exc")
+    void testInvalidDataType() {
+        triangle = new Triangle();
+        input = new String[]{"a","b","c","d"};
+        String combinedInputs = String.join(",", input);
+        inputArray = new ByteArrayInputStream(combinedInputs.getBytes());
+        System.setIn(inputArray);
+        assertThrows(IllegalArgumentException.class, triangle::getUserInput);
+
+    }
+
+    @Test
+    @DisplayName("Test for 3 invalid datatype input, throw IllegalArgument exc")
+    void test3InvalidDataType() {
+        triangle = new Triangle();
+        input = new String[]{"a","b","c"};
+        String combinedInputs = String.join(",", input);
+        inputArray = new ByteArrayInputStream(combinedInputs.getBytes());
+        System.setIn(inputArray);
+        assertThrows(IllegalArgumentException.class, triangle::getUserInput);
+
+    }
+
+    // testa rätt datatyp
+    @Test
+    @DisplayName("Test for valid input for SCALENE")
+    void testValidInputTypeS() {
+        triangle = new Triangle();
+        input = new String[]{"7","8","9"};
+        String combinedInputs = String.join(",", input);
+        inputArray = new ByteArrayInputStream(combinedInputs.getBytes());
+        System.setIn(inputArray);
+
+        triangle.getUserInput();
+
+        assertEquals(Triangle.TYPE.SCALENE, triangle.getCurrent_type());
+    }
+
+    @Test
+    @DisplayName("Test for valid input for EQUILATERAL")
+    void testValidInputTypeE() {
+        triangle = new Triangle();
+        input = new String[]{"8","8","8"};
+        String combinedInputs = String.join(",", input);
+        inputArray = new ByteArrayInputStream(combinedInputs.getBytes());
+        System.setIn(inputArray);
+
+        triangle.getUserInput();
+
+        assertEquals(Triangle.TYPE.EQUILATERAL, triangle.getCurrent_type());
+    }
+
+    @Test
+    @DisplayName("Test for valid input for ISOSCELES")
+    void testValidInputTypeI() {
+        triangle = new Triangle();
+        input = new String[]{"7","8","8"};
+        String combinedInputs = String.join(",", input);
+        inputArray = new ByteArrayInputStream(combinedInputs.getBytes());
+        System.setIn(inputArray);
+
+        triangle.getUserInput();
+
+        assertEquals(Triangle.TYPE.ISOSCELES, triangle.getCurrent_type());
+    }
+
+
+    // testa längd?
+
+
+    //testa empty??
+
+    @Test
+    @DisplayName("Test for 3 invalid datatype input, throw IllegalArgument exc")
+    void testEmptyArray() {
+        triangle = new Triangle();
+        input = new String[]{" "};
+        String combinedInputs = String.join(",", input);
+        inputArray = new ByteArrayInputStream(combinedInputs.getBytes());
+        System.setIn(inputArray);
+        assertThrows(IllegalArgumentException.class, triangle::getUserInput);
+
+    }
+
+
+    //testa null input värden
+
+    @Test
+    @DisplayName("Test for null array input, throw NullPointerException")
+    void testNullArrayInput() {
+        assertThrows(NullPointerException.class, () -> {
+            new Triangle((String[]) null);
+        });
+    }
+
+//    @Test
+//    @DisplayName("Test for wrong length input, throw Number exc")
+//    void testWrongDataType() {
+//        triangle = new Triangle();
+//        input = new String[]{"1", "2", "3"};
+//        String combinedInputs = String.join("\n", input);
+//        inputArray = new ByteArrayInputStream(combinedInputs.getBytes());
+//        System.setIn(inputArray);
+//        assertThrows(IllegalArgumentException.class, triangle::getUserInput);
+//
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    @Test
+//    @DisplayName("Test for no/null input, throw IllegalArgument exc")
+//    void testNullInput(){
+//        triangle = new Triangle();
+//        input = new String[] {"1","2"};
+//        String combinedInputs = String.join("\n", input);
+//        inputArray = new ByteArrayInputStream(combinedInputs.getBytes());
+//        System.setIn(inputArray);
+//        assertThrows(IllegalArgumentException.class, triangle::getUserInput);
+//
+//    }
+
+}
